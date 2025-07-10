2c02376b80cb2fd0e4b5de4d6f98d45bc21c5579
Eldeep1 <alieldeep111@gmail.com>
2025-07-10 16:25:33 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# clean_architecture_course
+
+here, I will be exploring the clean architecture on flutter and the bloc state management
---
+++ android/app/src/main/kotlin/com/depogramming/clean_architecture_course/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.depogramming.clean_architecture_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
