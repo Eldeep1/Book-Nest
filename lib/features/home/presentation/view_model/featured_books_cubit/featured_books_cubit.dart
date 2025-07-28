c7cd05dc76239ecbd3a5fb5f36aca981dd35ebe6
Eldeep1 <alieldeep111@gmail.com>
2025-07-28 17:52:34 +0300

    using homerepo

--- lib/features/home/presentation/view_model/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/view_model/featured_books_cubit/featured_books_cubit.dart
@@ -1,7 +1,18 @@
+import 'package:Bookly/features/home/data/repo/home_repo.dart';
 import 'package:Bookly/features/home/presentation/view_model/featured_books_cubit/featured_books_states.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class FeaturedBooksCubit extends Cubit<FeaturedBooksStates>{
-  FeaturedBooksCubit(super.FeaturedBooksInitial);
+class FeaturedBooksCubit extends Cubit<FeaturedBooksStates> {
+  FeaturedBooksCubit(super.initialState, {required this.homeRepo});
 
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoadingState());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) => emit(FeaturedBooksFailureState(failure.errorMessage)),
+      (books) => emit(FeaturedBooksSuccessState(books)),
+    );
+  }
 }
