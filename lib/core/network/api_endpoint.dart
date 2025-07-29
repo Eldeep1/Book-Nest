674317011eca69fcf060b440cd9b1ef839621373
Eldeep1 <alieldeep111@gmail.com>
2025-07-29 14:39:50 +0300

    implementing similar books

--- lib/core/network/api_endpoint.dart
+++ lib/core/network/api_endpoint.dart
@@ -1,4 +1,7 @@
 class ApiEndpoint{
   static const String newestBooksEndpoint="volumes?Filtering=free-ebooks&Sorting=newest &q=computer science";
   static const String featuredBooksEndpoint="volumes?Filtering=free-ebooks&q=subject:programming";
+  static String similarBooksEndpoint({required String category}){
+    return "volumes?Sorting=relevance&Filtering=free-ebooks&q=subject:$category";
+  }
 }
--- lib/features/home/data/repo/home_repo_impl.dart
+++ lib/features/home/data/repo/home_repo_impl.dart
@@ -55,4 +55,24 @@ class HomeRepoImpl implements HomeRepo {
       ));
     }
   }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks({required String category}) async{
+    try{
+      var data= await apiService.get(endPoint: ApiEndpoint.similarBooksEndpoint(category: category));
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    }
+    catch (e){
+      if(e is DioException){
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(
+          e.toString()
+      ));
+    }
+  }
 }
